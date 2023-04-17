ed43d0fb11cb5beef40a68603a7269cc262815c7
Sloth73 <Pichl.tomas.95@gmail.com>
2023-04-17 21:34:28 +0200

    Date on loan and money transfer between accs repaired

--- script.js
+++ script.js
@@ -23,24 +23,60 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: "Jessica Davis",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "CZK",
+  locale: "pt-PT", // de-DE
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: "Steven Thomas Williams",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "CZK",
+  locale: "pt-PT", // de-DE
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: "Sarah Smith",
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "CZK",
+  locale: "pt-PT", // de-DE
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -78,6 +114,7 @@ const displayMovements = function (acc, sort = false) {
   // Array is sorted before it's putted into forEach method if sort arg. is true
   ifSortMov.forEach(function (move, i) {
     const typeOfMovement = move > 0 ? `deposit` : `withdrawal`;
+    // Displaying date on every movement
     const date = new Date(acc.movementsDates[i]) //Looping through 2 arrays with same forEach method
     const dateArr = new Array(
       date.getDate(),
@@ -158,11 +195,15 @@ btnTransfer.addEventListener('click', function (event) {
   const recieveAccount = accounts.find(acc => acc.username === inputTransferTo.value);
   inputTransferAmount.value = inputTransferTo.value = '';
   if (amountOfMoney > 0 && currentAccount.balance >= amountOfMoney && recieveAccount.username !== currentAccount.username && recieveAccount) {
+    // Money transfer - movements array
     currentAccount.movements.push(-amountOfMoney);
     recieveAccount.movements.push(amountOfMoney);
+    // Date on money transfer
+    currentAccount.movementsDates.push(new Date());
+    recieveAccount.movementsDates.push(new Date());
 
     updateUserInterface(currentAccount);
-   }
+  }
 });
 
 // Loan request
@@ -170,7 +211,10 @@ btnLoan.addEventListener('click', function (event) {
   event.preventDefault();
   const amountOfLoanRequest = Number(inputLoanAmount.value);
   if (amountOfLoanRequest > 0 && currentAccount.movements.some(movement => movement >= amountOfLoanRequest * 0.1)) {
+    // Money transfer - movements array
     currentAccount.movements.push(amountOfLoanRequest);
+    // Date on money transfer
+    currentAccount.movementsDates.push(new Date());
   }
   updateUserInterface(currentAccount);
 })
